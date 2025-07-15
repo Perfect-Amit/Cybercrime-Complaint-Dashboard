6e06e0946ddfc09997a963dcb8cd984c5ea33a71
Amit <amitvijaynathtiwari08042005@gmail.com>
2025-07-15 14:05:08 +0530

    Delete complaints directory

--- complaints/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Complaint
-
-@admin.register(Complaint)
-class ComplaintAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'date_reported')
-    list_filter = ('category', 'date_reported')
-    search_fields = ('name', 'description', 'category')
-    ordering = ('-date_reported',)
